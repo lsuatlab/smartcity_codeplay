fe861013e1a70e31f1b189228ff7323bea2d83a2
Jacob Shelton <jakerish@gmail.com>
2018-07-02 01:49:51 -0500

    added trackball file, textboxs appear but coordinate frame conversion is hard so they aren't in the right place. Camera may be cause

--- mouseover support/script.js
+++ mouseover support/script.js
@@ -1,13 +1,14 @@
 			var camera, scene, raycaster, renderer, stats;
 			var mouse = new THREE.Vector2(), INTERSECTED;;
 			var oControls;
+			var text2;
 
 			init();
 			animate();
 
 			function init() {
 
-			  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
+			  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 5000 );
 
 			  scene = new THREE.Scene();
 			  scene.background = new THREE.Color( 0xf0f0f0 );
@@ -22,16 +23,30 @@
 				renderer.setPixelRatio( window.devicePixelRatio );
 				renderer.setSize( window.innerWidth, window.innerHeight );
 			  document.getElementById('container').appendChild(renderer.domElement);
+
+			  //stats, dont work 
 			  stats = new Stats();
 
-			  //orbit controls
+			  //orbit controls, dont work 
 				oControls = new THREE.OrbitControls( camera, renderer.domElement );
 
 			  window.addEventListener('resize', onWindowResize, false);
 			  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 			  document.addEventListener( 'mousedown', onDocumentMouseClick, false );
 			  document.addEventListener( 'keydown', onDocumentKeyDown, false );
-			  console.log(scene.children);
+			  text2 = document.createElement('div' );
+						text2.setAttribute("id", "nametag");
+						text2.style.position = 'absolute';
+						//text2.style.zIndex = 1;    // if you still don't see the label, try uncommenting this
+						text2.style.width = 100;
+						text2.style.height = 35;
+						text2.style.backgroundColor = "black";
+						text2.style.color = "white"
+						text2.innerHTML = "Hit any button to continue";
+						text2.style.top = 100 + 'px';
+						text2.style.left = 100 + 'px';
+						document.body.appendChild(text2);
+
 			}
 
 			function onWindowResize() {
@@ -49,6 +64,10 @@
 
 			function onDocumentMouseClick( event ) {
 				// find intersections
+				if(!!document.getElementById("nametag"))
+				{
+					document.body.removeChild(document.getElementById("nametag"));
+				}
 				camera.lookAt( scene.position );
 				camera.updateMatrixWorld();
 				oControls.reset();
@@ -57,23 +76,55 @@
 				var intersects = raycaster.intersectObjects( scene.children );
 				if ( intersects.length > 0 ) {
 					if ( INTERSECTED != intersects[ intersects.length -1 ].object ) {
-						if ( INTERSECTED ) INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+						if ( INTERSECTED ) {
+							INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+						}
 						INTERSECTED = intersects[ intersects.length -1 ].object;
 						INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
 						INTERSECTED.material.color.setHex( 0xff0000 );
+
+						var temp = deteremineScreenCoordinate(INTERSECTED);
+
+						text2 = document.createElement('div' );
+						text2.setAttribute("id", "nametag");
+						text2.style.position = 'absolute';
+						//text2.style.zIndex = 1;    // if you still don't see the label, try uncommenting this
+						text2.style.width = 100;
+						text2.style.height = 35;
+						text2.style.backgroundColor = "black";
+						text2.style.color = "white"
+						text2.innerHTML = "I wish I had brain cells";
+						text2.style.top = temp.x + 'px';
+						text2.style.left = temp.y + 'px';
+						document.body.appendChild(text2);
 					}
 				} else {
-					if ( INTERSECTED ) INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+					if ( INTERSECTED ) {
+						INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+					}
 					INTERSECTED = null;
 				}
 			}
 
+			//function from three.js cookbook github
+			//If I assume this works then my camera is really messed up
+			function deteremineScreenCoordinate(object) {
+        var vector = new THREE.Vector3();
+        vector.setFromMatrixPosition(object.matrixWorld);
+        vector.project(camera);
+        var width = window.innerWidth, height = window.innerHeight;
+        var widthHalf = width / 2, heightHalf = height / 2;
+        vector.x = ( vector.x * widthHalf ) + widthHalf;
+        vector.y = -( vector.y * heightHalf ) + heightHalf;
+        return vector;
+    	}
+
 			function onDocumentKeyDown( event ) {
-				var geometry = new THREE.CircleGeometry( Math.random() * 10, 32 );
+				var geometry = new THREE.CircleGeometry( Math.random() * 20, 32 );
 				var material = new THREE.MeshBasicMaterial( {color: Math.random() * 0xffffff} );
 				var sphere = new THREE.Mesh( geometry, material );
-			  sphere.position.x = camera.position.x + Math.random() * 20 -10;
-			  sphere.position.y = camera.position.y + Math.random() * 20 -10;
+			  sphere.position.x = camera.position.x + Math.random() * 120 -60;
+			  sphere.position.y = camera.position.y + Math.random() * 120 -60;
 			  sphere.position.z = camera.position.z -50;
 			  scene.add(sphere);
 			  if(scene.children.length == 2)
@@ -85,8 +136,7 @@
 			}
 
 			function animate() {
-				oControls.reset();
+				stats.update();
 			  requestAnimationFrame(animate);
 				renderer.render(scene, camera);
-				stats.update();
 			}
